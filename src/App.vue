740bc92c9a488d38c67e6832284c3cd4360556ef
JFYAzong <1018720708@qq.com>
2022-07-19 17:52:41 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <login />
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
